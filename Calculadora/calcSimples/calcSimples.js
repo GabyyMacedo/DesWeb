44a9f9f4413594defc88db3519e42788f576ab42
Gaby Macedo <135060010+GabyyMacedo@users.noreply.github.com>
2024-09-26 09:56:55 -0300

    Update calcSimples.js

--- Calculadora/calcSimples/calcSimples.js
+++ Calculadora/calcSimples/calcSimples.js
@@ -8,7 +8,12 @@ function calculadora(num1, num2, op){
             console.log(num1-num2)
             break
         case 'divisao':
-            console.log(num1/num2)
+            if (num2!==0){
+                console.log(num1/num2)
+            }
+            else{
+                console.log("Nao e possivel dividir por 0")
+            }
             break
         case 'multiplicacao':
             console.log(num1*num2)
